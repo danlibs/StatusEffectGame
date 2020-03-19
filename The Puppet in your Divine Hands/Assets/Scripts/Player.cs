878e54ab6bb1858dc7f11c8d0cd4cee5a9ea4523
Gameotix <61463699+Gameotix@users.noreply.github.com>
2020-03-18 21:59:55 -0300

    Dia 1
    - Inclusão do player e suas animações de caminhada, pulo, idle e hit (todas normais, sem fogo);
    - Sistema de movimentação básico e pulo.

---
+++ The Puppet in your Divine Hands/Assets/Scripts/Player.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float Speed;
+    public float JumpForce;
+    public Transform FeetPosition;
+    public float CheckRadius;
+    public LayerMask IsGround;
+    public float JumpTime;
+
+    private Rigidbody2D rb;
+    private Animator anim;
+    private float movement;
+    private bool isGrounded;
+    private bool jump = false;
+    private bool isJumping;
+    private bool facingRight;
+    private float jumpTimeCounter;
+
+    private void Awake()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+        anim = this.GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        movement = Input.GetAxis("Horizontal");
+        anim.SetInteger("Speed", Mathf.RoundToInt(movement));
+
+        isGrounded = Physics2D.OverlapCircle(FeetPosition.position, CheckRadius, IsGround);
+        anim.SetBool("Jump", !isGrounded);
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            jump = true;
+        }
+
+        if (movement > 0 && facingRight)
+        {
+            Flip();
+        }
+        else if (movement < 0 && !facingRight)
+        {
+            Flip();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(movement * Speed * Time.fixedDeltaTime, rb.velocity.y);
+
+        Vector2 jumpHeight = Vector2.up * (JumpForce + 5) * Time.fixedDeltaTime;
+        if (jump)
+        {
+            rb.AddForce(jumpHeight, ForceMode2D.Impulse);
+            isJumping = true;
+            jumpTimeCounter = JumpTime;
+            jump = false;
+        }
+
+        if (Input.GetKey(KeyCode.Space) && isJumping)
+        {
+            if (jumpTimeCounter > 0)
+            {
+                jumpTimeCounter -= Time.deltaTime;
+                rb.AddForce(jumpHeight + (jumpHeight * jumpTimeCounter), ForceMode2D.Impulse);
+                
+            }
+            else
+            {
+                isJumping = false;
+            }
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            isJumping = false;
+        }
+    }
+
+    private void Flip()
+    {
+        facingRight = !facingRight;
+        this.transform.Rotate(0, 180, 0);
+    }
+}
