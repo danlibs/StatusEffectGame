74e0356cfbc22339df20d1ce1f07927276acc4de
Gameotix <61463699+Gameotix@users.noreply.github.com>
2020-03-20 02:04:55 -0300

    Dia 2
    - Desenvolvimento dos sistemas de variação de massa e velocidade por meio de sliders no canvas;
    - Criação do sistema de "set on fire" (player perde vida nesse estado), incluindo animações;
    - Implementação do sistema de life e perda de vida;
    - Canvas completo.

---
+++ The Puppet in your Divine Hands/Assets/Scripts/Damage.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Damage : MonoBehaviour
+{
+    public int damage;
+
+    private Player player;
+
+    private void Start()
+    {
+        player = FindObjectOfType<Player>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            player.TookDamage(damage);
+        }
+    }
+}
--- The Puppet in your Divine Hands/Assets/Scripts/Player.cs
+++ The Puppet in your Divine Hands/Assets/Scripts/Player.cs
@@ -1,43 +1,65 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
     public float Speed;
     public float JumpForce;
+    public int MaxHealth;
     public Transform FeetPosition;
     public float CheckRadius;
     public LayerMask IsGround;
     public float JumpTime;
+    public Slider healthSlider;
+    public Slider speedSlider;
+    public Slider weightSlider;
+    public bool OnFire = false;
+    public bool Invencible;
 
     private Rigidbody2D rb;
     private Animator anim;
     private float movement;
+    private int currentHealth;
     private bool isGrounded;
     private bool jump = false;
     private bool isJumping;
     private bool facingRight;
     private float jumpTimeCounter;
+    private float timerDamage;
 
     private void Awake()
     {
         rb = this.GetComponent<Rigidbody2D>();
         anim = this.GetComponent<Animator>();
+        currentHealth = MaxHealth;
+        timerDamage = 2f;
+    }
+
+    private void Start()
+    {
+        healthSlider.value = currentHealth;
     }
 
     private void Update()
     {
+        anim.SetBool("OnFire", OnFire);
+        //Movimentação:
         movement = Input.GetAxis("Horizontal");
         anim.SetInteger("Speed", Mathf.RoundToInt(movement));
 
+        //Verificação de contato com o chão a partir de um círculo invisível:
         isGrounded = Physics2D.OverlapCircle(FeetPosition.position, CheckRadius, IsGround);
         anim.SetBool("Jump", !isGrounded);
+
+        //Detectão de input do pulo:
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
             jump = true;
         }
 
+        //Girar o personagem ao ir para a esquerda ou direita:
         if (movement > 0 && facingRight)
         {
             Flip();
@@ -46,12 +68,20 @@ public class Player : MonoBehaviour
         {
             Flip();
         }
+
+        //Se pegar fogo, perder vida constantemente.
+        if (OnFire)
+        {
+            ContinuousDamage();
+        }
     }
 
     private void FixedUpdate()
     {
+        //Realização da movimentação para a esquerda ou direita:
         rb.velocity = new Vector2(movement * Speed * Time.fixedDeltaTime, rb.velocity.y);
 
+        //Realização do pulo:
         Vector2 jumpHeight = Vector2.up * (JumpForce + 5) * Time.fixedDeltaTime;
         if (jump)
         {
@@ -61,29 +91,79 @@ public class Player : MonoBehaviour
             jump = false;
         }
 
+        //Quando mais tempo for apertada a tecla de pulo, mais alto o player pula:
         if (Input.GetKey(KeyCode.Space) && isJumping)
         {
             if (jumpTimeCounter > 0)
             {
                 jumpTimeCounter -= Time.deltaTime;
                 rb.AddForce(jumpHeight + (jumpHeight * jumpTimeCounter), ForceMode2D.Impulse);
-                
             }
             else
             {
                 isJumping = false;
             }
         }
-
         if (Input.GetKeyUp(KeyCode.Space))
         {
             isJumping = false;
         }
     }
 
+    //Implementação do giro do personagem:
     private void Flip()
     {
         facingRight = !facingRight;
         this.transform.Rotate(0, 180, 0);
+    }
+
+    //Receber dano de algo:
+    public void TookDamage(int damage)
+    {
+        if (!Invencible)
+        {
+            currentHealth -= damage;
+            rb.AddRelativeForce(new Vector2(-20, 10), ForceMode2D.Impulse);
+            anim.SetTrigger("Hit");
+            healthSlider.value = currentHealth;
+            Invencible = true;
+            Invoke("ResetInvulnerability", 2f);
+        }
+        
+    }
+
+    //Slider de controle da velocidade:
+    public void ChangeSpeed()
+    {
+        Speed = speedSlider.value;
+    }
+
+    //Slider de controle da massa:
+    public void ChangeWeight()
+    {
+        rb.mass = weightSlider.value;
+    }
+
+    //Botão para botar fogo no player:
+    public void SetOnFire()
+    {
+        OnFire = !OnFire;
+    }
+
+    //Implementação da diminuição de vida do player a cada 2 segundos enquanto pega fogo:
+    public void ContinuousDamage()
+    {
+        timerDamage -= Time.deltaTime;
+        if (timerDamage <= 0)
+        {
+            currentHealth--;
+            healthSlider.value = currentHealth;
+            timerDamage = 2f;
+        }
+    }
+
+    private void ResetInvulnerability()
+    {
+        Invencible = false;
     }
 }
